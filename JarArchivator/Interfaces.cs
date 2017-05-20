92f8fb42070d9820266dc8fd254009b24d6088f8
Evgenius2020 <spleen47@mail.ru>
2017-05-20 09:03:38 +0700

    Initial.

---
+++ JarArchivator/Interfaces.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace JarArchivator
+{
+    public interface IJarArchiveFile
+    {
+        string Name { get; }
+        string Path { get; }
+        long Size { get; }
+    }
+
+    public interface IJarArchive
+    {
+        IEnumerable<IJarArchiveFile> Files { get; }
+        void Unpack(string targetFolder);
+    }
+
+    public interface IJarArchivator
+    {
+        IJarArchive Pack(string sourceFolder, string archiveFileName);
+        IJarArchive Open(string archiveFileName);
+    }
+}
---
+++ JarArchivator/JarArchiver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JarArchivator
+{
+    public class JarArchiver : IJarArchivator
+    {
+        public IJarArchive Open(string archiveFileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IJarArchive Pack(string sourceFolder, string archiveFileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
